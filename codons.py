927c3d14dd553e1829bc3297e6e41a2c4c212930
haley641 <81654096+haley641@users.noreply.github.com>
2021-04-10 18:19:39 -0700

    Add files via upload

---
+++ codons.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Print out all the codons for the sequence below in reading frame 1
+# Use a 'for' loop
+
+dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
+for i in range(0, len(dna)-2,3):
+	print(dna[i:i+3])
+
+
+
+"""
+python3 codons.py
+ATA
+GCG
+AAT
+ATC
+TCT
+CAT
+GAG
+AGG
+GAA
+"""
